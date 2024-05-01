8705c519d2b5f309708e11a003c15ab773f94790
Joao Paulo <js0918990@gmail.com>
2024-05-01 12:25:39 -0300

    Atualizando fotos

---
+++ javascript/funcionImg.js
@@ -0,0 +1,16 @@
+// Selecionando todos os botões
+let btns = document.querySelectorAll('.gallery-card-btn');
+
+// Adicionando evento de clique para cada botão
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        // Obtendo o caminho da imagem do atributo "data-img" do botão
+        let imagePath = btn.getAttribute('data-img');
+
+        // Obtendo o div da imagem correspondente ao botão clicado
+        let divCard = btn.parentElement;
+
+        // Alterando o fundo do div para a nova imagem
+        divCard.style.backgroundImage = `url(../img/${imagePath})`;
+    });
+});
